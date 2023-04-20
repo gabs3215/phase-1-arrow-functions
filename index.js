a0773e6b04442fcad998c9bcbd782ca696143e4a
Gabriella Romero <rgabriella752@gmail.com>
2023-04-20 01:50:30 -0700

    save changes

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(){
+   let a = 2000
+   let b = 100
+    return (a/b)
+}
+const square = (x = 2) => {
+    return (x * x)
+}
+const add = (a = 3, b= 4) => {
+    return (a + b)
+}
+
+
